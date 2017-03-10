c37f485ec1c37c198c4cd2729184791b316edfc0
Rémi Fruteau de Laclos <remi.fruteaudelaclos@axa.fr>
2017-03-10 08:42:07 +0100

    chore(perf): big performances improvements

--- content.js
+++ content.js
@@ -1,109 +1,111 @@
-$(function() {
-    if ($("body").attr("id") === "jira") {
-        console.log("[jira-chrome-extension] Current page is a JIRA page.");
-
-        function dragInterval($ghxPool) {
-            let maxLoop = 20;
-            const interval = setInterval(function() {
-                const $columns = $ghxPool.find(".ghx-drag-in-progress .ghx-zone-overlay-column");
-                if($columns.length > 0) {
-                    $ghxPool.scrollLeft($ghxPool.scrollLeft()+1);
-                    $ghxPool.scrollLeft($ghxPool.scrollLeft()-1);
-                    $columns.css({
-                        "width": `${width?width:200}px`
-                    });
-                    clearInterval(interval);
-                }
-                if(maxLoop-- <= 0) {
-                    clearInterval(interval);
-                }
-            }, 50);
-        }
-
-        function dragEndInterval($this) {
-            let maxLoop = 20;
-            const interval = setInterval(function() {
-                if(!$this.is(':visible')) {
-                    clearInterval(interval);
-                    loadingInterval();
-                }
-                if(maxLoop-- <= 0) {
-                    clearInterval(interval);
-                }
-            }, 500);
-        }
-
-        function loadingInterval() {
-            let maxLoop = 20;
-            const interval = setInterval(function() {
-                if($('body.ghx-loading-pool').length === 0) {
-                    clearInterval(interval);
-                    fixWidth();
-                }
-                if(maxLoop-- <= 0) {
-                    clearInterval(interval);
-                }
-            }, 100);
-        }
-        const $ghxPool = $('#ghx-pool');
-        $ghxPool.on('mousedown', '.ui-draggable, .js-parent-drag', function() {
-            const $this = $(this);
-            dragInterval($ghxPool);
-            dragEndInterval($this);
-        });
-
-        $('.ghx-swimlane-header').click(function() {
-            fixWidth();
-        });
-    }
-});
-let width = 200;
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if(request.type === "refresh") {
-        if(request.value) {
-            width = request.value;
-        }
-        fixWidth();
-    }
-});
-
-function fixWidth() {
+(function() {
     $(function() {
-        const $ghxPoolColumn = $("#ghx-pool-column");
-        if($ghxPoolColumn.length > 0) {
+        if ($("body").attr("id") === "jira") {
+            console.log("[jira-chrome-extension] Current page is a JIRA page.");
 
-            const $ghxPool = $ghxPoolColumn.find("#ghx-pool");
-            $ghxPool.css({
-                "padding-top": 0,
-                "height": "100%"
-            });
-            $ghxPool.find("li").css({
-                "width": `${width?width:200}px`
-            });
+            function dragInterval($ghxPool) {
+                let maxLoop = 20;
+                const interval = setInterval(function() {
+                    const $columns = $ghxPool.find(".ghx-drag-in-progress .ghx-zone-overlay-column");
+                    if($columns.length > 0) {
+                        $ghxPool.scrollLeft($ghxPool.scrollLeft()+1);
+                        $ghxPool.scrollLeft($ghxPool.scrollLeft()-1);
+                        $columns.css({
+                            "width": `${width?width:200}px`
+                        });
+                        clearInterval(interval);
+                    }
+                    if(maxLoop-- <= 0) {
+                        clearInterval(interval);
+                    }
+                }, 50);
+            }
 
-            followScroll($ghxPool);
-            $ghxPool.off('scroll').on('scroll', function() {
+            function dragEndInterval($this) {
+                let maxLoop = 20;
+                const interval = setInterval(function() {
+                    if(!$this.is(':visible')) {
+                        clearInterval(interval);
+                        loadingInterval();
+                    }
+                    if(maxLoop-- <= 0) {
+                        clearInterval(interval);
+                    }
+                }, 500);
+            }
+
+            function loadingInterval() {
+                let maxLoop = 20;
+                const interval = setInterval(function() {
+                    if(!$('body').hasClass('ghx-loading-pool')) {
+                        clearInterval(interval);
+                        fixWidth();
+                    }
+                    if(maxLoop-- <= 0) {
+                        clearInterval(interval);
+                    }
+                }, 100);
+            }
+            const $ghxPool = $('#ghx-pool-column #ghx-pool');
+            $ghxPool.on('scroll', function() {
                 followScroll($(this));
             });
-
-            const $ghxColumnHeaderGroup = $ghxPool.find("#ghx-column-header-group");
-            
-            const $firstSwimlane = $ghxPool.find('.ghx-swimlane.ghx-first');
-            $firstSwimlane.css({
-                "padding-top": `${$ghxColumnHeaderGroup.height()}px`
+            $ghxPool.on('mousedown', '.ui-draggable, .js-parent-drag', function() {
+                const $this = $(this);
+                dragInterval($ghxPool);
+                dragEndInterval($this);
             });
 
-            const $ghxDetailContents = $('#ghx-detail-contents');
-            $ghxDetailContents.css({
-                "position": "fixed",
-                "z-index": 2
+            $(document).click(function() {
+                loadingInterval();
+                fixWidth();
             });
-
-            const $uiDraggable = $ghxPool.find(".ui-draggable, .js-parent-drag");
-            $uiDraggable.attr('jira-chrome-extension', 'done');
+        }
+    });
+    let width = 200;
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if(request.type === "refresh") {
+            if(request.value) {
+                width = request.value;
+            }
+            fixWidth();
         }
     });
 
+    function fixWidth() {
+        $(function() {
+            const $ghxPoolColumn = $("#ghx-pool-column");
+            if($ghxPoolColumn.length > 0) {
+
+                const $ghxPool = $ghxPoolColumn.find("#ghx-pool");
+                $ghxPool.css({
+                    "padding-top": 0,
+                    "height": "100%"
+                });
+                $ghxPool.find("li").css({
+                    "width": `${width?width:200}px`
+                });
+
+                followScroll($ghxPool);
+
+                const $ghxColumnHeaderGroup = $ghxPool.find("#ghx-column-header-group");
+                
+                const $firstSwimlane = $ghxPool.find('.ghx-swimlane.ghx-first');
+                $firstSwimlane.css({
+                    "padding-top": `${$ghxColumnHeaderGroup.height()}px`
+                });
+
+                const $ghxDetailContents = $('#ghx-detail-contents');
+                $ghxDetailContents.css({
+                    "position": "fixed",
+                    "z-index": 2
+                });
+
+                const $uiDraggable = $ghxPool.find(".ui-draggable, .js-parent-drag");
+                $uiDraggable.attr('jira-chrome-extension', 'done');
+            }
+        });
+    }
     function followScroll($ghxPool) {
         const $ghxColumnHeaderGroup = $ghxPool.find("#ghx-column-header-group");
         $ghxColumnHeaderGroup.css({
@@ -129,4 +131,4 @@ function fixWidth() {
             "left": `-${$ghxPool.scrollLeft()}px`
         });
     }
-}
+})();
